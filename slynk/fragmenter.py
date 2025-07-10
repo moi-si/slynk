43a8f3639f08632d56922a994fed1f92e088175b
Moi <111831296+moi-si@users.noreply.github.com>
2025-07-10 15:24:55 +0800

    Optimize fragmenter

--- slynk/fragmenter.py
+++ slynk/fragmenter.py
@@ -8,22 +8,21 @@ logger = logger.getChild('fragmenter')
 
 def fragment_content(data: str, num: int) -> list:
     """
-    frag <data> into num pieces
+    fragment <data> into num pieces
     """
-    data_length = len(data)
-    fragmented_content = []
-    if len(data) > 1:
-        dividing_points = random.sample(
-            range(1, data_length), min(num, data_length - 1)
-        )
-    else:
-        fragmented_content.append(data)
-        return
+    if (data_length := len(data)) <= 1:
+        return [data]
+    dividing_points = random.sample(
+        range(1, data_length), min(num, data_length - 1)
+    )
     dividing_points.append(0)
     dividing_points.append(data_length)
     dividing_points.sort()
+    fragmented_content = []
     for i in range(len(dividing_points) - 1):
-        fragmented_content.append(data[dividing_points[i]:dividing_points[i + 1]])
+        fragmented_content.append(
+            data[dividing_points[i]:dividing_points[i + 1]]
+        )
     return fragmented_content
 
 def fragment_pattern(data, pattern, len_sni: int, num_pieces: int):
@@ -32,27 +31,26 @@ def fragment_pattern(data, pattern, len_sni: int, num_pieces: int):
     the first part of the pattern contains in
     fragmented_data[0]
     """
-    fragmented_data = []
     position = data.find(pattern)
-    logger.debug(f"{pattern} {position}")
+    logger.debug('%s %d', pattern, position)
     if position == -1:
         return fragment_content(data, 2 * num_pieces)
 
     pattern_length = len(pattern)
     data_length = len(data)
 
-    fragmented_data.extend(fragment_content(data[0:position], num_pieces))
+    fragmented_data = fragment_content(data[:position], num_pieces)
 
     l = len(fragmented_data)
 
-    if len_sni >= len(pattern) / 2:
-        len_sni = int(len(pattern) / 2)
-        logger.info("len_sni too big so it has been set to %d", len_sni)
-
-    num = int(pattern_length / len_sni)
-
-    if num * len_sni < pattern_length:
-        num += 1
+    if len_sni > len(pattern) // 2:
+        len_sni = len(pattern) // 2
+        num = 2
+        logger.info("len_sni too big so it has been set to %d.", len_sni)
+    else:
+        num = pattern_length // len_sni
+        if num * len_sni < pattern_length:
+            num += 1
 
     for i in range(num):
         fragmented_data.append(
@@ -79,29 +77,31 @@ async def send_chunks(writer, data, sni):
         base_header = data[:3]
         record = data[5:]
 
-        splited_tls_data, l, r = fragment_pattern(
+        fragmented_tls_data, l, r = fragment_pattern(
             record, sni, policy["len_tls_sni"], policy["num_tls_pieces"]
         )
         tcp_data = b''
-        for i, _ in enumerate(splited_tls_data):
-            tmp = splited_tls_data[i] = (
+        for i, _ in enumerate(fragmented_tls_data):
+            tmp = fragmented_tls_data[i] = (
                 base_header
-                + int.to_bytes(len(splited_tls_data[i]), byteorder="big", length=2)
-                + splited_tls_data[i]
+                + int.to_bytes(
+                    len(fragmented_tls_data[i]), byteorder='big', length=2
+                )
+                + fragmented_tls_data[i]
             )
             tcp_data += tmp
-            logger.debug('Added chunk: of %d bytes.', len(tmp))
-            # logger.debug('Added chunk: %s', tmp)
+            logger.debug('Added piece: %d bytes.', len(tmp))
+            # logger.debug('Added piece: %s', tmp)
 
-        logger.info('TLS chunked: %d bytes.', len(tcp_data))
-        logger.debug('TLS chunked: %s', repr(tcp_data))
+        logger.info('TLS fragmented: %d bytes.', len(tcp_data))
+        # logger.debug('TLS fragmented: %s', repr(tcp_data))
 
         lenl = 0
         for i in range(l):
-            lenl += len(splited_tls_data[i])
+            lenl += len(fragmented_tls_data[i])
         lenr = lenl
         for i in range(l, r):
-            lenr += len(splited_tls_data[i])
+            lenr += len(fragmented_tls_data[i])
 
         splited_tcp_data, l, r = fragment_pattern(
             tcp_data,
@@ -114,15 +114,17 @@ async def send_chunks(writer, data, sni):
             writer.write(packet)
             await writer.drain()
             logger.debug(
-                "TCP sent: %d bytes. And 'll sleep for %f seconds.",
+                "TCP sent: %d bytes. And 'll sleep for %s seconds.",
                 len(packet),
-                policy["send_interval"],
+                policy["send_interval"]
             )
             # logger.debug("TCP sent: %s", repr(packet))
             await asyncio.sleep(policy["send_interval"])
 
+        logger.info('All the chunks have been sent.')
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 0)
-        logger.debug('Successfully send all the chunks.')
 
     except Exception as e:
-        logger.error('Failed to send chunks due to %s', repr(e))
+        logger.error(
+            'Failed to send chunks due to %s.', repr(e), exc_info=True
+        )
